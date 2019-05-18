e2151c4657cbc7ed1ee74a3d5269379e03220c43
darkflame265 <42233002+darkflame265@users.noreply.github.com>
2019-05-18 20:38:59 +0900

    master
    it's add unity file

---
+++ Assets/#Script/ColumnMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColumnMove : MonoBehaviour
+{
+
+    float makeTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        makeTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //만들어진 기둥을 5초후  삭제해서 기둥이 쌓이지 않게함
+        if(Time.time - makeTime > 5f)
+        Destroy(gameObject);
+    }
+}
